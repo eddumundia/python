ccca7be2e7e10645a8fa3b8db15b049c3ba48892
Edward Mundia <emundia@kemri-wellcome.org>
2025-01-14 15:02:16 +0300

    Refactored the function

--- Classes/classes.py
+++ Classes/classes.py
@@ -3,6 +3,33 @@ class Employee:
         self.name = name
         self.age = age
         self.position = position
-        self.slalary = salary
+        self.set_salary(salary)
 
-employee1 = Employee("Edward Mundia", 37, "Senior software developer", 1200)
+    def increase_salary(self, percent):
+        self.salary += self.salary * (percent/100)
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old. Employee is a {self.position} with the salary of ${self.salary}"
+
+    def __repr__(self):
+        return (
+            f"Employee("
+            f"{repr(self.name)}, {repr(self.age)}, "
+            f"{repr(self.position)}, {repr(self.salary)})"
+        )
+
+    @property
+    def salary(self):
+        return self._salary
+
+    def set_salary(self, salary):
+        if salary < 1000:
+            raise ValueError('Minimum wage is $1000')
+        self._salary = salary
+
+
+employee1 = Employee("Ji-Soo", 38, "developer", 1200)
+employee2 = Employee("Lauren", 44, "tester", 1000)
+
+employee1.set_salary(2000)
+print(employee1.salary)
