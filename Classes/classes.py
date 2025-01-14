9f635299c7c67701870b574f5836eabb409f7087
Edward Mundia <emundia@kemri-wellcome.org>
2025-01-14 14:43:41 +0300

    Learning OOP classes

---
+++ Classes/classes.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self, name, age, position, salary):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.slalary = salary
+
+employee1 = Employee("Edward Mundia", 37, "Senior software developer", 1200)
